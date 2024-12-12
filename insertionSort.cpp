fd5d8108b60741a805df8b03da675af6e6a33cae
Adnan <64307271+Adnann07@users.noreply.github.com>
2024-12-12 11:35:38 +0600

    Add files via upload

--- insertionSort.cpp
+++ insertionSort.cpp
@@ -1,32 +1,60 @@
-#include <iostream>
+#include<bits/stdc++.h>
 using namespace std;
-int insertionSort(int a[],int n)
+
+int binarySearch(const vector<int>& a,int key,int left,int right)
+
 {
+    while(left<=right)
+    {
+        int mid=left+(left-right)/2;
+        if(a[mid]==key) return mid+1;
+        else if(a[mid]<key) left=mid+1;
+        else right=mid-1;
 
-    for(int j=1;j<n;j++)
-    {
-        int key=a[j];
-        int i=j-1;
     }
-    while(i>=0 && a[i]>key)
+    return left;
+}
+
+void insertionSort(vector<int> &a)
+{
+    for(int i=1;i<a.size();i++)
     {
-        a[i+1]=a[i];
-        i--;
+        int key=a[i];
+        int pos=binarySearch(a,key,0,i-1);
+
+         for(int j=i-1;j>=pos;j--)
+    {
+        a[j+1]=a[j];
     }
+    a[pos]=key;
+    }
+
 
 }
 
 
-int main() {
-    int arr[] = {12, 11, 13, 5, 6};
-    int n = sizeof(arr) / sizeof(arr[0]);
-
-    insertionSort(arr, n);
-
-    cout << "Sorted array: ";
-    for (int i = 0; i < n; i++) {
-        cout << arr[i] << " ";
+int main()
+{
+    int n;
+    cin>>n;
+    vector<int> v(n);
+    for(int i=0;i<n;i++)
+    {
+        cin>>v[i];
     }
-    return 0;
-}
 
+    for(int num:v)
+    {
+        cout<<num<<" ";
+    }
+    cout<<endl;
+
+    insertionSort(v);
+
+    cout<<"Sorted array: ";
+    for(int num:v)
+    {
+        cout<<num<<" ";
+    }
+    cout<<endl;
+}
