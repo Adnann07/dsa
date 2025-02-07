69f287efd57783d7108d473fd3fab6a0af12c24d
Adnan <64307271+Adnann07@users.noreply.github.com>
2025-02-07 22:56:47 +0600

    Add files via upload

--- c3.cpp
+++ c3.cpp
@@ -1,52 +1,9 @@
 #include<bits/stdc++.h>
 using namespace std;
+int main()
+{
 
+    cout<<"CESFUW"<<endl;
 
-int binarySearchRecursive(vector<int>& v, int left, int right, int key) {
-
-
-  if(left>right)
-  {
-      return -1;
-  }
-  int mid=left+(right-left)/2;
-
-  if(v[mid]==key)
-  {
-      return key;
-  }
-
-  else if(v[mid]>key)
-  {
-      return binaryRecursiveSearch(v,mid+1,right,key)
-  }
-
-
-
-   else{
-    return binarySearchRecursive(v,left,mid-1,key);
-   }
 }
 
-int main() {
-    int n;
-    cin >> n;
-    vector<int> v(n);
-    for (int i = 0; i < n; i++) {
-        cin >> v[i];
-    }
-    int key;
-    cin >> key;
-
-
-    int result=binaryRecursiveSearch(v,0,n-1,key);
-
-
-    if (result != -1) {
-        cout << "Found in " << result << " position" << endl;
-    } else {
-        cout << "Not found" << endl;
-    }
-
-    return 0;
-}
